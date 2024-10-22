0cb11136a4b1d4816f9c6c2bfd70e7b0a5fbc6b0
Maryam-Ameen <155364505+maryamameen34@users.noreply.github.com>
2024-10-22 11:03:33 +0500

    Update index.js

--- server/index.js
+++ server/index.js
@@ -22,7 +22,7 @@ app.use(bodyParser.json({ limit: '50mb' }));
 app.use(bodyParser.urlencoded({ extended: true, limit: "50mb" }));
 app.use(morgan("dev"));
 app.use(cors({
-    origin: "http://localhost:3000",
+    origin: "https://ecomerace-project-i8zh.vercel.app/",
     credentials: true
 }));
 
