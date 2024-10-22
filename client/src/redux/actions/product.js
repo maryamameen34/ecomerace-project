10702453f762c1b49e55e4b909aac8f2c5bc75ea
Maryam-Ameen <155364505+maryamameen34@users.noreply.github.com>
2024-10-22 11:12:44 +0500

    Update product.js

--- client/src/redux/actions/product.js
+++ client/src/redux/actions/product.js
@@ -5,7 +5,7 @@ import { GET_ALL_PRODUCTS_FAILURE, GET_ALL_PRODUCTS_REQUEST, GET_ALL_PRODUCTS_SU
 // Upload Product Action
 export const uploadProductAction = createAsyncThunk('product/upload', async (formData, { rejectWithValue }) => {
     try {
-        const response = await axios.post("http://localhost:8000/api/upload-product", formData, {
+        const response = await axios.post("https://ecomerace-project-orpin.vercel.app/?vercelToolbarCode=4H22x3-yGKOe0Lk/api/upload-product", formData, {
             headers: {
                 'Content-Type': 'multipart/form-data',
             },
@@ -20,7 +20,7 @@ export const uploadProductAction = createAsyncThunk('product/upload', async (for
 // Fetch Products Action
 export const fetchProductsAction = createAsyncThunk('product/fetchAll', async (_, { rejectWithValue }) => {
     try {
-        const response = await axios.get("http://localhost:8000/api/products", {
+        const response = await axios.get("https://ecomerace-project-orpin.vercel.app/?vercelToolbarCode=4H22x3-yGKOe0Lk/api/products", {
             withCredentials: true,
         });
         return response.data.products; // Assuming the backend returns an array of products under 'products'
@@ -34,7 +34,7 @@ export const getProductDetails = (id) => async (dispatch) => {
     try {
         dispatch({ type: 'GET_PRODUCT_DETAILS_REQUEST' });
 
-        const { data } = await axios.get(`http://localhost:8000/api/product/${id}`, { withCredentials: true });
+        const { data } = await axios.get(`https://ecomerace-project-orpin.vercel.app/?vercelToolbarCode=4H22x3-yGKOe0Lk/api/product/${id}`, { withCredentials: true });
 
         dispatch({
             type: 'GET_PRODUCT_DETAILS_SUCCESS',
